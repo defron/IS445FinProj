d4ebad1d550fb8d9343c3294fc339a6744aba5f7
Kevin Thomer <webreg@defron.org>
2013-12-13 20:45:55 -0800

    Finished requirements

--- finproj/Controllers/HomeController.cs
+++ finproj/Controllers/HomeController.cs
@@ -16,5 +16,59 @@ namespace finproj.Controllers
             return View();
         }
 
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Absolute()
+        {
+            return View();
+        }
+
+        public ActionResult Static()
+        {
+            return View();
+        }
+
+        public ActionResult Float()
+        {
+            return View();
+        }
+
+        public ActionResult Client()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult ServerSide()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ServerSide(string inputNumber)
+        {
+            if (string.IsNullOrEmpty(inputNumber) || (inputNumber.Length != 10))
+            {
+                //shouldn't be possible anymore
+                return Content("invalid input, please try again");
+            }
+            else
+            {
+                string formatted = formatNumber(inputNumber);
+                return View("ServerResult", (object)formatted);
+            }
+        }
+
+        private string formatNumber(string inputNumber)
+        {
+            return String.Format("formated number is: ({0}) {1}-{2}", inputNumber.Substring(0, 3), inputNumber.Substring(3, 3), inputNumber.Substring(6, 4));
+        }
     }
 }
